a3259678f9baf7affbb155fd2afd3c1f1c2f6219
ovh <oveach@gmail.com>
2014-10-01 14:23:57 +0200

    Mise en place des bases du site pour la partie 2 du cours

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new OC\PlatformBundle\OCPlatformBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
