0c3d2b1297b98689fbe8795725a0b58a8aaf85b1
ovh <oveach@gmail.com>
2014-10-02 13:19:32 +0200

    Ajout d'un menu pour tout le site

--- src/OC/CoreBundle/Controller/DefaultController.php
+++ src/OC/CoreBundle/Controller/DefaultController.php
@@ -11,13 +11,27 @@ class DefaultController extends Controller
     {
         // truc pour éviter le problème du trailing slash...
         // si on détecte un / final, on redirige vers la même url sans / final...
+        // sauf pour l'url racine de prod (uniquement /)
         $requestUri = $request->getRequestUri();
-        if (strrpos($requestUri, "/") === strlen($requestUri) - 1) {
+        if (strlen($requestUri) > 1 and strrpos($requestUri, "/") === strlen($requestUri) - 1) {
             $url = rtrim($requestUri, '/');
             return $this->redirect($url, 301);
         }
 
         return $this->render('OCCoreBundle:Default:index.html.twig');
+    }
+    
+    public function menuAction($activeRoute)
+    {
+        $menuItems = array(
+            "Accueil" => 'oc_core_homepage',
+            "Annonces" => 'oc_platform_home',
+            "Contact" => 'oc_core_contact',
+        );
+        return $this->render('OCCoreBundle:Default:menu-global.html.twig', array(
+            'menuItems' => $menuItems,
+            'active_route' => $activeRoute,
+        ));
     }
 
     public function contactAction()
