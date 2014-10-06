e6decaad31adeea84ad68a57d7a7a317ed1c37de
ovh <oveach@gmail.com>
2014-10-06 13:34:24 +0200

    Renommage du controleur en CoreBundle:CoreController

--- src/OC/CoreBundle/Controller/CoreController.php
+++ src/OC/CoreBundle/Controller/CoreController.php
@@ -5,7 +5,7 @@ namespace OC\CoreBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
-class DefaultController extends Controller
+class CoreController extends Controller
 {
     public function indexAction(Request $request)
     {
